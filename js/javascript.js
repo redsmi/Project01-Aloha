505a7c817959d1c6fa506d7c949e7e17604b07a1
David Yin <davidyin92@hotmail.com>
2018-03-06 13:59:11 -0800

    JS flickity and email submit improvement

--- js/javascript.js
+++ js/javascript.js
@@ -1,20 +1,20 @@
-console.log('Hello script this is working yeah')
+$( document ).ready(function(){
 
-$('.main-carousel').flickity({
-    // options
-    cellAlign: 'left',
-    contain: true
-  });
-  
-    $("#newsletter").on("submit",function(){
-    var emailSub;
-    emailSub = $('input').val();
-    console.log(emailSub);
-
+  $('.main-carousel').flickity({
+      // options
+      cellAlign: 'left',
+      contain: true,
+      prevNextButtons: false,
+      autoPlay: true
+    });
+    
+  $("#newsletter").on("submit",function(event){
+    event.preventDefault(); 
     if ($("#your-email").val() == "") {
       alert("Please submit a valid email address.");
-      return false;
     } else {
       alert("Thanks for subscribing!");
     }
   });
+
+});
